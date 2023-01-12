18544e3f0ed0e73c5fc90aaac678849c98b709f1
Daniela Aedo <daedovil@nttdata.com>
2023-01-12 19:08:05 -0300

    add post controller

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -2,32 +2,74 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiForbiddenResponse, ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { PostEntity } from './entities/post.entity';
 
 @Controller('post')
+@ApiTags('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
+  @ApiCreatedResponse({ 
+    type: PostEntity, 
+    description: 'The post has been successfully deleted' 
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiNotFoundResponse({ description: 'User Not Found' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   create(@Body() createPostDto: CreatePostDto) {
     return this.postService.create(createPostDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: PostEntity })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiNotFoundResponse({ description: 'User Not Found' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   findAll() {
     return this.postService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: PostEntity })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiNotFoundResponse({ description: 'User Not Found' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   findOne(@Param('id') id: string) {
     return this.postService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({ 
+    type: PostEntity, 
+    description: 'The post has been successfully updated' 
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiNotFoundResponse({ description: 'User Not Found' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     return this.postService.update(+id, updatePostDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ 
+    type: PostEntity, 
+    description: 'The post has been successfully deleted' 
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiNotFoundResponse({ description: 'User Not Found' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   remove(@Param('id') id: string) {
     return this.postService.remove(+id);
   }
