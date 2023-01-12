540cdd1915d5e6e7d9a39371f0e2da7c0a705074
Daniela Aedo <daedovil@nttdata.com>
2023-01-12 19:07:04 -0300

    add post entity

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,1 +1,13 @@
-export class Post {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Post } from "@prisma/client";
+
+export class PostEntity implements Post {
+    @ApiProperty({ default: 1 })
+    id: number;
+    @ApiProperty({ default: 'The Cat' })
+    title: string;
+    @ApiProperty({ default: 'The cat (Felis catus) is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae.' })
+    content: string;
+    @ApiProperty({ default: true })
+    published: boolean;
+}
