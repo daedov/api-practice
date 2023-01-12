07a36538c425cb2f22b500eedce89b96a70d1826
Daniela Aedo <daedovil@nttdata.com>
2023-01-12 19:08:45 -0300

    add post service

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class PostService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+
+  constructor(private prisma: PrismaService) {}
+
+  async create(createPostDto: CreatePostDto) {
+    try {
+      const post = await this.prisma.post.create({ data: createPostDto });
+      return post;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all post`;
+  async findAll() {
+    try {
+      const posts = await this.prisma.post.findMany();
+      return posts;
+    } catch (error) {
+      throw new InternalServerErrorException(error);    
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: number) {
+    const post = await this.prisma.post.findUnique({ where: { id } });
+    if (!post) {
+      throw new NotFoundException(`Id ${id} Not Found`);
+    }
+    return post;
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    await this.findOne(id);
+    const post = await this.prisma.post.update({
+      where: { id },
+      data: updatePostDto,
+    });
+    return post;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async remove(id: number) {
+    await this.findOne(id);
+    const post = await this.prisma.post.delete({ where: { id } });
+    return post;
   }
 }
