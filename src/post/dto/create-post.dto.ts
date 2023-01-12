d829a57a83a6c8970be30e63cf92ea883520679d
Daniela Aedo <daedovil@nttdata.com>
2023-01-12 19:06:21 -0300

    add create post dto

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,17 @@
-export class CreatePostDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
+
+export class CreatePostDto {
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    title: string;
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    content: string;
+    @IsNotEmpty()
+    @IsBoolean()
+    @ApiProperty()
+    published: boolean;
+}
