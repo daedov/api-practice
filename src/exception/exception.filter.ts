1530151a0999f5578eaf53af7a3cec365abe3d49
Daniela Aedo <daedovil@nttdata.com>
2023-01-16 13:37:28 -0300

    add exception filter

---
+++ src/exception/exception.filter.ts
@@ -0,0 +1,30 @@
+/*
+https://docs.nestjs.com/exception-filters#exception-filters-1
+*/
+
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+@Catch(NotFoundException, InternalServerErrorException)
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+    const status  = exception.getStatus();
+    const message = exception.message;
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      message: message,
+      path: request.url,
+    });
+  }
+}
